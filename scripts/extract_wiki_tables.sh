138451c1189abd94689995d8132e51f742b0341f
sythello <sythello@163.com>
2020-10-23 22:16:58 -0700

    bash script modify

--- scripts/extract_wiki_tables.sh
+++ scripts/extract_wiki_tables.sh
@@ -9,10 +9,15 @@ CLASSPATH=contrib/wiki_extractor/tableBERT-1.0-SNAPSHOT-jar-with-dependencies.ja
 # You may need to use the full Wikipedia dump for data extraction, for example,
 # https://dumps.wikimedia.org/enwiki/20201001/enwiki-20201001-pages-articles-multistream.xml.bz2 .
 # In our paper we used the dump `enwiki-20190520-pages-articles-multistream.xml.bz2`.
-wget -nc https://dumps.wikimedia.org/enwiki/20200901/enwiki-20200901-pages-articles-multistream1.xml-p1p30303.bz2 -P data/datasets/
-WIKI_DUMP=data/datasets/enwiki-20200901-pages-articles-multistream1.xml-p1p30303.bz2
 
-OUTPUT_FILE=data/preprocessed_data/wiki_tables.jsonl
+# wget -nc https://dumps.wikimedia.org/enwiki/20200901/enwiki-20200901-pages-articles-multistream1.xml-p1p30303.bz2 -P data/datasets/
+wget -nc https://dumps.wikimedia.org/enwiki/20201001/enwiki-20201001-pages-articles-multistream.xml.bz2 \
+-P /Users/mac/Desktop/syt/Deep-Learning/Dataset/TaBERT_datasets/WikiTables/raw/
+
+WIKI_DUMP=/Users/mac/Desktop/syt/Deep-Learning/Dataset/TaBERT_datasets/WikiTables/raw/enwiki-20201001-pages-articles-multistream.xml.bz2
+
+# OUTPUT_FILE=data/preprocessed_data/wiki_tables.jsonl
+OUTPUT_FILE=/Users/mac/Desktop/syt/Deep-Learning/Dataset/TaBERT_datasets/WikiTables/wiki_tables.jsonl
 
 CLASSPATH=${CLASSPATH} JAVA_PATH=${JAVA_PATH} python \
     -m preprocess.extract_wiki_data \
--- scripts/preprocess_commoncrawl_tables.sh
+++ scripts/preprocess_commoncrawl_tables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-CW_DATA_PATH=data/datasets/commoncrawl.sample.jsonl
-OUTPUT_FILE=data/preprocessed_data/common_crawl.preprocessed.jsonl
+# CW_DATA_PATH=data/datasets/commoncrawl.sample.jsonl
+CW_DATA_PATH=/Users/mac/Desktop/syt/Deep-Learning/Dataset/TaBERT_datasets/CommonCrawl/raw/
+
+# OUTPUT_FILE=data/preprocessed_data/common_crawl.preprocessed.jsonl
+OUTPUT_FILE=/Users/mac/Desktop/syt/Deep-Learning/Dataset/TaBERT_datasets/CommonCrawl/common_crawl.preprocessed.jsonl
+
 FILTER="[0-1][0-9].tar.gz"
+# FILTER="00.tar.gz"
 
 python \
     -m preprocess.common_crawl \
